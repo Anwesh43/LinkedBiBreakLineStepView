7e4d656bbf1f1f3249a1a16002a84e3858f6117b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-22 10:15:16 +0530

    adding BiBreakLineStep class

--- bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
+++ bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
@@ -169,4 +169,28 @@ class BiBreakLineStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiBreakLineStep(var i : Int) {
+
+        private val root : BBLNode = BBLNode(0)
+        private var curr : BBLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update { i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

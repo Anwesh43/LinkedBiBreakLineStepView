5deb1cf1c286f1f4dade7f1122b21b49dd561b3a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-22 09:13:19 +0530

    adding libraries and defininf constants

--- bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
+++ bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bibreaklinestepview
 /**
  * Created by anweshmishra on 22/02/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")

17630adbc5934075b0a838d0f7baf9e42445cda0
Anwesh43 <anweshthecool0@gmail.com>
2019-02-22 10:00:32 +0530

    ading drawBBLNode canvas extension method

--- bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
+++ bibreaklinestepview/src/main/java/com/anwesh/uiprojects/bibreaklinestepview/BiBreakLineStepView.kt
@@ -14,6 +14,7 @@ import android.view.MotionEvent
 
 val nodes : Int = 5
 val lines : Int = 4
+val parts : Int = 2
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val sizeFactor : Float = 2.9f
@@ -26,4 +27,36 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * this
+
+fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val yGap : Float = size / lines
+    val dSize : Float = size / 2
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(parts - 1)) {
+        val scj : Float = sc2.divideScale(j, parts)
+        save()
+        rotate(-90f * j.sf() * scj)
+        drawLine(0f, 0f, 0f, -size, paint)
+        for (k in 0..(lines - 1)) {
+            val sck : Float = sc1.divideScale(k, lines)
+            save()
+            translate(0f, -yGap * k * sck)
+            drawLine(0f, 0f, dSize / 2 * j.sf(), 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}

9388ee9e142b38575b1df109b41cec87bb764e04
Anwesh43 <anweshthecool0@gmail.com>
2019-02-22 10:26:35 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbibreaklinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbibreaklinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbibreaklinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bibreaklinestepview.BiBreakLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiBreakLineStepView.create(this)
     }
 }
